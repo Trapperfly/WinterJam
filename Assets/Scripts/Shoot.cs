aff44e17da52fb40adb860679125bd450b7ddad9
Gabriel Spangelid Johnsbråten <trapperfly@gmail.com>
2023-12-06 16:13:08 +0100

    Added fire rate

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
+    [SerializeField] float fireRate;
     [SerializeField] GameObject prefab;
     [SerializeField] float force;
     [SerializeField] Transform shootPoint;
+    float timer;
+    private void Start()
+    {
+        timer = 60 / fireRate;
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButton(0))
-        {
-            Rigidbody snowball = Instantiate(prefab, shootPoint.position, Quaternion.Euler(Random.Range(0,361), Random.Range(0, 361), Random.Range(0, 361))).GetComponent<Rigidbody>();
-            snowball.AddForce(shootPoint.forward * force, ForceMode.Impulse);
+        {   
+            if (timer >= 60 / fireRate)
+            {
+                Rigidbody snowball = Instantiate(prefab, shootPoint.position, Quaternion.Euler(Random.Range(0, 361), Random.Range(0, 361), Random.Range(0, 361))).GetComponent<Rigidbody>();
+                snowball.AddForce(shootPoint.forward * force, ForceMode.Impulse);
+                timer = 0;
+            }
         }
+        timer += 60 * Time.deltaTime;
     }
 }
